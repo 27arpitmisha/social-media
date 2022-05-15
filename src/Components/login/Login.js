ed21bb6ad0eaeb05fefbccc5e2502bf55604b3d0
Arpit Mishra <27arpitmishra@gmail.com>
2022-05-16 00:19:38 +0530

    UI changes

--- src/Components/login/Login.js
+++ src/Components/login/Login.js
@@ -2,6 +2,33 @@ import React from 'react'
 import { useNavigate } from 'react-router'
 import { useAuth } from '../../Context/UserAuthenticationContext';
 import GoogleButton from 'react-google-button';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function Copyright(props) {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
+          Your Website
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+  
+  const theme = createTheme();
 export default function Login() {
     const navigate = useNavigate();
     const { login, googleAuthentication } = useAuth();
@@ -21,15 +48,78 @@ export default function Login() {
         }
     }
     return (
-        <div>Login
-            <button onClick={loginHandler}>Login with test values</button>
-            <GoogleButton
-                onClick={googleLoginHandler}
-                // onSuccess={responseGoogle}
-                // onFailure={responseGoogle}
-                // cookiePolicy={'single_host_origin'}
-            />
-            <button onClick={() => { navigate('/signup') }}>Register</button>
-        </div>
+        <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={loginHandler}
+              >
+                Sign In
+              </Button>
+              
+              <Grid container>
+  
+                <Grid item onClick={() => { navigate('/signup') }}>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>  
+          </Box>
+          <GoogleButton style={{marginTop: '20px' }} onClick={googleLoginHandler}/>
+          <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+      </ThemeProvider>
+        // <div>Login
+        //     <button onClick={loginHandler}>Login with test values</button>
+        //     <GoogleButton
+        //         onClick={googleLoginHandler}
+        //         // onSuccess={responseGoogle}
+        //         // onFailure={responseGoogle}
+        //         // cookiePolicy={'single_host_origin'}
+        //     />
+        //     <button onClick={() => { navigate('/signup') }}>Register</button>
+        // </div>
     )
 }
